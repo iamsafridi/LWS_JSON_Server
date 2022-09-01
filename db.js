dcc652790a0f5c0e92fe79156d853b866456d76d
shahid Afridi <shahidafridi.baymax@gmail.com>
2022-09-01 22:46:28 +0600

    complete

---
+++ db.js
@@ -0,0 +1,14 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const videos2 = require("./data/videos2");
+const transactions = require("./data/transactions");
+const tags = require("./data/tags");
+
+module.exports = {
+  todos,
+  videos,
+  videos2,
+  transactions,
+  tags,
+}
+
